a463ebfa01d5a97106cd65c938abd552a03d9ed4
diater <diater@outlook.com>
2026-06-17 22:42:44 +0800

    refactor: move experience collection systems to experience/collection.rs

--- src/systems/experience/mod.rs
+++ src/systems/experience/mod.rs
@@ -2,3 +2,8 @@ pub mod approval;
 pub mod collection;
 pub mod governance;
 pub mod writeback;
+
+pub(crate) use collection::{
+    experience_collection_completion_system, experience_collection_workitem_system,
+    task_terminated_experience_trigger_system,
+};
